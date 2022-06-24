96418d7ba1750b38ffde9279f3bbdcf68944aab0
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-06-24 13:57:34 +0200

    Fixed types in factory steps

--- src/logistikoptimierung/Entities/FactoryObjects/FactoryStepTypes.java
+++ src/logistikoptimierung/Entities/FactoryObjects/FactoryStepTypes.java
@@ -2,12 +2,12 @@ package logistikoptimierung.Entities.FactoryObjects;
 
 public class FactoryStepTypes {
 
-    public static final String GetMaterialFromSuppliesAndMoveBackToWarehouse = "Get Material From Supplies And Move back to Warehouse";
-    public static final String MoveMaterialFromTransporterToWarehouse = "Move Material From Transporter To Warehouse";
-    public static final String MoveMaterialsForProductFromWarehouseToInputBuffer = "Move Materials For Product From Warehouse To InputBuffer";
+    public static final String GetMaterialFromSuppliesAndMoveBackToWarehouse = "Get material from supplier and move back to warehouse";
+    public static final String MoveMaterialFromTransporterToWarehouse = "Move material from transporter to warehouse";
+    public static final String MoveMaterialsForProductFromWarehouseToInputBuffer = "Move materials for product from warehouse to input buffer";
     public static final String Produce = "Produce";
-    public static final String MoveProductToOutputBuffer = "Move Product To Output Buffer";
-    public static final String MoveProductFromOutputBufferToWarehouse = "Move Product From Output Buffer To Warehouse";
-    public static final String ConcludeOrderTransportToCustomer = "Conclude the order and transport products from order from warehouse to customer";
+    public static final String MoveProductToOutputBuffer = "Move product to output buffer";
+    public static final String MoveProductFromOutputBufferToWarehouse = "Move product from output buffer to warehouse";
+    public static final String ConcludeOrderTransportToCustomer = "Conclude the order and transport products for order from warehouse to customer";
 
 }
