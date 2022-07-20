3e6f7eb0b13c34c48795c65df15bf60f5ad87335
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-07-20 19:28:22 +0200

    Added java doc to material class

--- src/logistikoptimierung/Entities/WarehouseItems/Material.java
+++ src/logistikoptimierung/Entities/WarehouseItems/Material.java
@@ -7,6 +7,15 @@ public class Material extends WarehouseItem {
     private final String[] transportTypes;
     private final String engine;
 
+    /**
+     * creates a material object
+     * @param materialId sets material id
+     * @param name sets the name of the material
+     * @param area sets the area for transportation
+     * @param transportTypes sets the transport types for transportation
+     * @param engine sets the engine for transportation
+     * @param travelTime sets the travel time for transportation
+     */
     public Material(String materialId, String name, String area, String[] transportTypes, String engine,  int travelTime)
     {
         super(materialId, name, WarehouseItemTypes.Material);
