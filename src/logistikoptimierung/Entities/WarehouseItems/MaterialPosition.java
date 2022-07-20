75c6f52ed651a42a74e5cf3eb474f9cfc8c468b0
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-07-20 19:29:46 +0200

    Added java doc to material position class

--- src/logistikoptimierung/Entities/WarehouseItems/MaterialPosition.java
+++ src/logistikoptimierung/Entities/WarehouseItems/MaterialPosition.java
@@ -1,5 +1,11 @@
 package logistikoptimierung.Entities.WarehouseItems;
 
+/**
+ * Create a material position which handles the warehouse item and the amount.
+ * Material Positions are used for transport and in the warehouse
+ * @param item the item in the material position
+ * @param amount the amount of the item in the material postion
+ */
 public record MaterialPosition(WarehouseItem item, int amount) {
 
     @Override
