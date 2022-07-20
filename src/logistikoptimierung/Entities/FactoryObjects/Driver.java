9c9e561fc34d9ade9a20433dfe3816490927655d
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-07-20 15:47:14 +0200

    Added java doc information for driver

--- src/logistikoptimierung/Entities/FactoryObjects/Driver.java
+++ src/logistikoptimierung/Entities/FactoryObjects/Driver.java
@@ -1,41 +1,43 @@
-package logistikoptimierung.Entities.FactoryObjects;
+/**
+ * This class creates a Driver for a Transporter. It does not support any specific tasks.
+ * If the driver is assigned to a Transporter the transporter object sets the blocked time to the same value as the
+ * transporter.
+ */
 
-import logistikoptimierung.Entities.WarehouseItems.WarehouseItem;
+package logistikoptimierung.Entities.FactoryObjects;
 
 public class Driver extends FactoryObject
 {
-    private FactoryStepTypes currentTask;
     private long blockedUntilTimeStep;
 
-
+    /**
+     * Creates an object of the type driver.
+     * @param name sets the name
+     * @param id sets the id
+     */
     public Driver(String name, int id) {
         super(name, "D" + id, FactoryObjectTypes.Driver);
         blockedUntilTimeStep = 0;
     }
 
-    @Override
-    public boolean doWork(long currentTimeStep, WarehouseItem item, int amountOfItems, FactoryStepTypes stepType)
-    {
-        if(currentTimeStep < blockedUntilTimeStep)
-        {
-            super.addBlockMessage(super.getName(), currentTask);
-            return false;
-        }
-
-        super.addLogMessage("Driving for item: " + item.getName());
-        return true;
-    }
-
+    /**
+     * Resets the object and sets the blocked until time step back to 0.
+     */
     public void resetDriver()
     {
         this.blockedUntilTimeStep = 0;
-        this.currentTask = FactoryStepTypes.None;
     }
 
+    /**
+     * @return returns the time step until the driver is blocked
+     */
     public long getBlockedUntilTimeStep() {
         return blockedUntilTimeStep;
     }
 
+    /**
+     * @param blockedUntilTimeStep Sets the time step until the driver is blocked
+     */
     public void setBlockedUntilTimeStep(long blockedUntilTimeStep)
     {
         this.blockedUntilTimeStep = blockedUntilTimeStep;
