81c71886360056476b973cff0a9181d8b045257b
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-07-26 15:55:46 +0200

    Added java doc to warehouse types

--- src/logistikoptimierung/Entities/WarehouseItems/WarehouseItemType.java
+++ src/logistikoptimierung/Entities/WarehouseItems/WarehouseItemType.java
@@ -1,8 +1,20 @@
 package logistikoptimierung.Entities.WarehouseItems;
 
+/**
+ * Types of the different warehouse items
+ */
 public enum WarehouseItemType
 {
+    /**
+     * Indicates a material
+     */
     Material,
+    /**
+     * Indicates a product
+     */
     Product,
+    /**
+     * Indicates an order
+     */
     Order;
 }
