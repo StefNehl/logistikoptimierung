27180ba2d0cb2d9e345c08cacf0d788a419dbd81
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-07-26 16:54:31 +0200

    Added missing java doc to interfaces

--- src/logistikoptimierung/Contracts/IDataService.java
+++ src/logistikoptimierung/Contracts/IDataService.java
@@ -2,6 +2,15 @@ package logistikoptimierung.Contracts;
 
 import logistikoptimierung.Entities.Instance;
 
+/**
+ * Interface for the implementation of different data loading services. These are used to generate or load data for an
+ * instance to optimize.
+ */
 public interface IDataService {
+    /**
+     * Loads the data with the data service
+     * @param filename gives the file name of the contract list
+     * @return an instance for the optimization and simulation
+     */
     Instance loadData(String filename);
 }
--- src/logistikoptimierung/Contracts/IOptimizationService.java
+++ src/logistikoptimierung/Contracts/IOptimizationService.java
@@ -3,6 +3,14 @@ import logistikoptimierung.Entities.FactoryObjects.FactoryStep;
 
 import java.util.List;
 
+/**
+ * Interface for an optimization service. The instance which gets optimized are given via the constructor.
+ */
 public interface IOptimizationService {
+    /**
+     * Optimize the given instance
+     * @param nrOfOrdersToOptimize nr of orders which should be optimized.
+     * @return a list of factory steps for the simulation
+     */
     List<FactoryStep> optimize(int nrOfOrdersToOptimize);
 }
