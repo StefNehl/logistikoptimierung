9c2a07d09db471d0f2a24a0daa8fe90e5cf89d1a
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-07-26 16:50:15 +0200

    Added missing java doc to driver pool item and instance

--- src/logistikoptimierung/Entities/Instance.java
+++ src/logistikoptimierung/Entities/Instance.java
@@ -5,6 +5,11 @@ import logistikoptimierung.Entities.WarehouseItems.Order;
 
 import java.util.List;
 
+/**
+ * Record which stores the object for an instance to optimize
+ * @param factory for which the optimization should be done
+ * @param orderList for which the optimization should be done
+ */
 public record Instance (Factory factory, List<Order> orderList)
 {
 
--- src/logistikoptimierung/Services/EnumeratedCalculation/DriverPoolItem.java
+++ src/logistikoptimierung/Services/EnumeratedCalculation/DriverPoolItem.java
@@ -3,5 +3,10 @@ package logistikoptimierung.Services.EnumeratedCalculation;
 import logistikoptimierung.Entities.FactoryObjects.Driver;
 import logistikoptimierung.Entities.FactoryObjects.Transporter;
 
+/**
+ * Record for the driver pool which is used in the enumerated calculation
+ * @param driver driver in the pool item
+ * @param transporter transporter in the pool item
+ */
 public record DriverPoolItem(Driver driver, Transporter transporter) {
 }
