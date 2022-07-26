e05971a69a96fb87ddda2feda9431de3cdd163b2
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-07-26 16:17:17 +0200

    Added missing java doc to transport planning item

--- src/logistikoptimierung/Services/FirstComeFirstServeOptimizer/TransporterPlanningItem.java
+++ src/logistikoptimierung/Services/FirstComeFirstServeOptimizer/TransporterPlanningItem.java
@@ -2,25 +2,41 @@ package logistikoptimierung.Services.FirstComeFirstServeOptimizer;
 
 import logistikoptimierung.Entities.FactoryObjects.Transporter;
 
+/**
+ * Planning item for the transporters
+ */
 public class TransporterPlanningItem
 {
     private long blockedTime = 0;
     private Transporter transporter;
 
+    /**
+     * Creates a planning item for the transporter
+     * @param transporter transporter which is planned
+     */
     public TransporterPlanningItem(Transporter transporter)
     {
         this.blockedTime = 0;
         this.transporter = transporter;
     }
 
+    /**
+     * @return the transporter
+     */
     public Transporter getTransporter() {
         return transporter;
     }
 
+    /**
+     * @return the blocked time
+     */
     public long getBlockedTime() {
         return blockedTime;
     }
 
+    /**
+     * @param timeToAdd increases the blocked time of the planning item
+     */
     public void increaseBlockedTime(long timeToAdd)
     {
         blockedTime += timeToAdd;
