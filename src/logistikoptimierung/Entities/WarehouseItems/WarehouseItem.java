f41a5e6c19ba77fb2896f25223b32a3482ea4188
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-07-26 15:58:42 +0200

    Added java doc to WarehouseItem

--- src/logistikoptimierung/Entities/WarehouseItems/WarehouseItem.java
+++ src/logistikoptimierung/Entities/WarehouseItems/WarehouseItem.java
@@ -1,11 +1,21 @@
 package logistikoptimierung.Entities.WarehouseItems;
 
+/**
+ * Indicates a warehouse item in the simulation (Material, Product, Order)
+ * This class should only  be extended by a different class.
+ */
 public class WarehouseItem {
 
     private final String name;
     private final String itemId;
     private WarehouseItemType itemType;
 
+    /**
+     * Creates a warehouse item with an id, name and the item type
+     * @param itemId indicates the id
+     * @param name indicates the name
+     * @param itemType indicates the item type
+     */
     public WarehouseItem(String itemId, String name, WarehouseItemType itemType)
     {
         this.name = name;
@@ -13,14 +23,23 @@ public class WarehouseItem {
         this.itemType = itemType;
     }
 
+    /**
+     * @return the name of the warehouse item
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return the id of the item
+     */
     public String getItemId() {
         return itemId;
     }
 
+    /**
+     * @return the item type of the warehouse item
+     */
     public WarehouseItemType getItemType() {
         return itemType;
     }
