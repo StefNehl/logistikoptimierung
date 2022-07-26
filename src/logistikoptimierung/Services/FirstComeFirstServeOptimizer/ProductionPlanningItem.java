f0595881cf67d58de0a312749b1f8e0401a5a995
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-07-26 16:19:54 +0200

    Added missing java doc to production planning item

--- src/logistikoptimierung/Services/FirstComeFirstServeOptimizer/ProductionPlanningItem.java
+++ src/logistikoptimierung/Services/FirstComeFirstServeOptimizer/ProductionPlanningItem.java
@@ -2,25 +2,42 @@ package logistikoptimierung.Services.FirstComeFirstServeOptimizer;
 
 import logistikoptimierung.Entities.FactoryObjects.Production;
 
+/**
+ * A planning item for the production
+ */
 public class ProductionPlanningItem
 {
-    private long blockedTime = 0;
-    private Production production;
+    private long blockedTime;
+    private final Production production;
 
+    /**
+     * Creates a new production planning item
+     * @param production production to plan
+     */
     public ProductionPlanningItem(Production production)
     {
         this.blockedTime = 0;
         this.production = production;
     }
 
+    /**
+     * @return the production which is planned
+     */
     public Production getProduction() {
         return production;
     }
 
+    /**
+     * @return the blocked time of the planning item
+     */
     public long getBlockedTime() {
         return blockedTime;
     }
 
+    /**
+     * Increases the blocked time
+     * @param timeToAdd increase the blocked time with the time to add
+     */
     public void increaseBlockedTime(long timeToAdd)
     {
         blockedTime += timeToAdd;
