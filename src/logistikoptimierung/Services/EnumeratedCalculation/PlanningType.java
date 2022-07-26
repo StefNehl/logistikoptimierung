c0432462fbbc08e284aeb6039fa0370b4588b8ef
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-07-26 16:46:09 +0200

    Added missing java doc to planning type

--- src/logistikoptimierung/Services/EnumeratedCalculation/PlanningType.java
+++ src/logistikoptimierung/Services/EnumeratedCalculation/PlanningType.java
@@ -1,8 +1,20 @@
 package logistikoptimierung.Services.EnumeratedCalculation;
 
+/**
+ * Different types of planning
+ */
 public enum PlanningType
 {
+    /**
+     * Planning type for acquiring material
+     */
     Acquire,
+    /**
+     * Planning type for producing a product
+     */
     Produce,
+    /**
+     * Planning type for delivering an item to the customer
+     */
     Deliver
 }
