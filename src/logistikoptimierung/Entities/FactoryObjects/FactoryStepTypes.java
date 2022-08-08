61fae2ea9151d1eddb7b2875416f94be7e30384e
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-08-08 17:14:54 +0200

    Added javadoc to factory steps

--- src/logistikoptimierung/Entities/FactoryObjects/FactoryStepTypes.java
+++ src/logistikoptimierung/Entities/FactoryObjects/FactoryStepTypes.java
@@ -10,35 +10,48 @@ public enum FactoryStepTypes
      */
     None,
     /**
-     * Get material from supplier and move back to warehouse
+     * Get material from supplier and move back to warehouse. Needs a Factory Object Transporter, the warehouse item material to gather
+     * and the amount to gather from the material.
      */
     GetMaterialFromSuppliesAndMoveBackToWarehouse,
     /**
-     * Move material from transporter to warehouse
+     * Move material from transporter to warehouse. Needs a Factory Object Transporter, the warehouse item material to move
+     * and the amount to move from the material. The Transporter needs the warehouse item stored with the amount and the capacity
+     * in the warehouse needs to be higher or equal than the amount. Otherwise, the step will fail.
      */
     MoveMaterialFromTransporterToWarehouse,
     /**
-     * Move materials for product from warehouse to input buffer
+     * Move materials for product from warehouse to input buffer. Needs a Factory Object Production, the product which this production
+     * should produce and the amount of batches (currently only 1 is supported). Every material needed for the production needs to
+     * be in the warehouse, otherwise the step will fail
      */
     MoveMaterialsForProductFromWarehouseToInputBuffer,
     /**
-     * Produce
+     * Produce. Need a Factory object Production, and alle the materials for the production in the Input buffer of this production.
+     * Amount is the nr of batches (currently only 1 is supported).
      */
     Produce,
     /**
-     * Move product to output buffer
+     * Move product to output buffer. Needs the Factory Object Production, the produced product and the nr of Batches (only 1).
+     * The output buffer needs to have the space for the product, otherwise the step will fail.
      */
     MoveProductToOutputBuffer,
     /**
-     * Move product from output buffer to warehouse
+     * Move product from output buffer to warehouse. Needs the Factory Object, the product in the output buffer and the nr of Batches (only 1).
+     * The warehouse needs to have remaining capacity, otherwise the step will fail.
      */
     MoveProductFromOutputBufferToWarehouse,
     /**
-     * Move the order and transport products for order from warehouse to customer
+     * Move the order and transport products for order from warehouse to customer. Needs the Factory Object Transporter,
+     * the warehouse item Order which needs to be handled and the amount of the needed product or material in the order.
+     * The amount of the material or product needs to be in the warehouse, otherwise this step will fail.
      */
     ConcludeOrderTransportToCustomer,
     /**
-     * Transporter closes order from the customer
+     * Transporter closes order from the customer. Needs the Factory Object Transporter, and the warehouse item Order to close.
+     * The parameter Amount is ignored in the step. This step will also increase the income of the factory. This step will fail,
+     * if the needed amount of the Order > 0. Furthermore, if the amount in the Order was not deduct to zero by the ConcludeOrderTransportToCustomer
+     * Step.
      */
     ClosesOrderFromCustomer;
 }
